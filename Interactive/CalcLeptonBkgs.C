82b9357baa0a32948e14e388f9ea731edbef3bb7
Jeff Ouellette <jeff.ouellette@cern.ch>
2020-03-04 13:36:07 -0500

    Finalized Z boson background studies

--- Interactive/CalcLeptonBkgs.C
+++ Interactive/CalcLeptonBkgs.C
@@ -7,20 +7,24 @@
 #include "TTree.h"
 #include "TChain.h"
 
+#include <fstream>
+
 void CalcLeptonBkgs () {
 
   TChain* chain = new TChain ("PbPbZTrackTree", "PbPbZTrackTree");
-  double ttbarCounts = 0, ZtautauCounts = 0, allCounts = 0;
+  double ttbarEECounts = 0, ttbarMuMuCounts = 0, ZtautauEECounts = 0, ZtautauMuMuCounts = 0, eeAllCounts = 0, mumuAllCounts = 0;
 
   chain->Add ("/atlasgpfs01/usatlas/data/jeff/ZTrackAnalysis/rootFiles/MCAnalysis/Nominal/PbPb18_pp_ttbar/*.root");
   cout << "Running over ttbar events..." << endl;
-  ttbarCounts = chain->GetEntries ();
+  ttbarEECounts = chain->GetEntries ("isEE");
+  ttbarMuMuCounts = chain->GetEntries ("!isEE");
   chain->Reset ();
 
 
   chain->Add ("/atlasgpfs01/usatlas/data/jeff/ZTrackAnalysis/rootFiles/MCAnalysis/Nominal/PbPb18_pp_Ztautau/*.root");
   cout << "Running over ttbar events..." << endl;
-  ZtautauCounts = chain->GetEntries ();
+  ZtautauEECounts = chain->GetEntries ("isEE");
+  ZtautauMuMuCounts = chain->GetEntries ("!isEE");
   chain->Reset ();
 
 
@@ -33,25 +37,47 @@ void CalcLeptonBkgs () {
   chain->Add ("/atlasgpfs01/usatlas/data/jeff/ZTrackAnalysis/rootFiles/MCAnalysis/Nominal/PbPb18_pp_Zee/Group*.root");
   chain->Add ("/atlasgpfs01/usatlas/data/jeff/ZTrackAnalysis/rootFiles/MCAnalysis/Nominal/PbPb18_pp_Zmumu/Group*.root");
   cout << "Running over all events..." << endl;
-  allCounts = chain->GetEntries ();
+  eeAllCounts = chain->GetEntries ("isEE");
+  mumuAllCounts = chain->GetEntries ("!isEE");
   chain->Reset ();
 
+  ofstream of;
+  of.open ("DileptonBkgs.out");
 
-  const double allEffXS = allCounts * 642.820 / (415887 + 638346 + 638321 + 976912 + 357861 + 541661 + 561006 + 840470);
-  const double ttbarEffXS = ttbarCounts * 56.695 * 0.54439 / 67734;
-  const double ZtautauEffXS = ZtautauCounts * 642.820 / 193395;
 
-  cout << "effective cross sections are" << endl;
-  cout << "all Zs         = " << allEffXS << endl;
-  cout << "ttbar bkg      = " << ttbarEffXS << endl;
-  cout << "Z->tautau bkg  = " << ZtautauEffXS << endl;
+  const double eeEffXS = eeAllCounts * 642.820 / (415887 + 638346 + 638321 + 976912);
+  const double ttbarEEEffXS = ttbarEECounts * 56.695 * 0.54439 / 67734;
+  const double ZtautauEEEffXS = ZtautauEECounts * 642.820 / 193395;
 
-  cout << "# ttbar events     = " << ttbarCounts << endl;
-  cout << "# Z->tautau events = " << ZtautauCounts << endl;
-  cout << "# events           = " << allCounts << endl;
+  const double mumuEffXS = mumuAllCounts * 642.820 / (357861 + 541661 + 561006 + 840470);
+  const double ttbarMuMuEffXS = ttbarMuMuCounts * 56.695 * 0.54439 / 67734;
+  const double ZtautauMuMuEffXS = ZtautauMuMuCounts * 642.820 / 193395;
 
-  cout << "ttbar bkg rate     = " << (ttbarEffXS / allEffXS) * 100 << "%" << endl;
-  cout << "Z->tautau bkg rate = " << (ZtautauEffXS / allEffXS) * 100 << "%" << endl;
+  of << "electron effective cross sections are" << endl;
+  of << "all Z->ee         = " << eeEffXS << endl;
+  of << "ttbar->ee bkg      = " << ttbarEEEffXS << endl;
+  of << "Z->tautau->ee bkg  = " << ZtautauEEEffXS << endl;
+  of << endl;
+  of << "# ttbar->ee events     = " << ttbarEECounts << endl;
+  of << "# Z->tautau->ee events = " << ZtautauEECounts << endl;
+  of << "# Z->ee events         = " << eeAllCounts << endl;
+  of << endl;
+  of << "ttbar->ee bkg rate     = " << (ttbarEEEffXS / eeEffXS) * 100 << "%" << endl;
+  of << "Z->tautau->ee bkg rate = " << (ZtautauEEEffXS / eeEffXS) * 100 << "%" << endl;
+
+  of << endl << endl;
+
+  of << "muon effective cross sections are" << endl;
+  of << "all Z->mumu         = " << mumuEffXS << endl;
+  of << "ttbar->mumu bkg      = " << ttbarMuMuEffXS << endl;
+  of << "Z->tautau->mumu bkg  = " << ZtautauMuMuEffXS << endl;
+  of << endl;
+  of << "# ttbar->mumu events     = " << ttbarMuMuCounts << endl;
+  of << "# Z->tautau->mumu events = " << ZtautauMuMuCounts << endl;
+  of << "# Z->mumu events         = " << mumuAllCounts << endl;
+  of << endl;
+  of << "ttbar->mumu bkg rate     = " << (ttbarMuMuEffXS / mumuEffXS) * 100 << "%" << endl;
+  of << "Z->tautau->mumu bkg rate = " << (ZtautauMuMuEffXS / mumuEffXS) * 100 << "%" << endl;
 }
 
 #endif
--- Interactive/CalcQCDBkgs.C
+++ Interactive/CalcQCDBkgs.C
@@ -3,9 +3,13 @@
 
 #include "Params.h"
 
-#include "TFile.h"
-#include "TTree.h"
-#include "TChain.h"
+#include "AtlasUtils.h"
+
+#include <TFile.h>
+#include <TTree.h>
+#include <TChain.h>
+#include <TH1D.h>
+#include <TCanvas.h>
 
 void CalcQCDBkgs () {
 
@@ -27,59 +31,86 @@ void CalcQCDBkgs () {
   dataTree->SetBranchAddress ("z_m",          &z_m);
   dataTree->SetBranchAddress ("event_weight", &event_weight);
 
-  mcTree->SetBranchAddress ("isQCDBkg",   &isQCDBkg);
-  mcTree->SetBranchAddress ("isSameSign", &isSameSign);
-  mcTree->SetBranchAddress ("isEE",       &isEE);
-  mcTree->SetBranchAddress ("z_m",        &z_m);
+  mcTree->SetBranchAddress ("isQCDBkg",     &isQCDBkg);
+  mcTree->SetBranchAddress ("isSameSign",   &isSameSign);
+  mcTree->SetBranchAddress ("isEE",         &isEE);
+  mcTree->SetBranchAddress ("z_m",          &z_m);
   mcTree->SetBranchAddress ("event_weight", &event_weight);
 
   float n_os_mumu = 0;
   float n_ss_mumu = 0;
 
-  TH1D* h_zm_ee_os_data = new TH1D ("h_zm_ee_os_data", ";#it{m}_{#it{ee}} [GeV];Events", 75, 60, 135);
-  TH1D* h_zm_ee_ss_data = new TH1D ("h_zm_ee_ss_data", ";#it{m}_{#it{ee}} [GeV];Events", 75, 60, 135);
-  TH1D* h_zm_ee_qcd_data = new TH1D ("h_zm_ee_qcd_data", ";#it{m}_{#it{ee}} [GeV];Events", 75, 60, 135);
-  TH1D* h_zm_ee_os_mc = new TH1D ("h_zm_ee_os_mc", ";#it{m}_{#it{ee}} [GeV];Events", 75, 60, 135);
-  TH1D* h_zm_ee_ss_mc = new TH1D ("h_zm_ee_ss_mc", ";#it{m}_{#it{ee}} [GeV];Events", 75, 60, 135);
-  TH1D* h_zm_ee_qcd_mc = new TH1D ("h_zm_ee_qcd_mc", ";#it{m}_{#it{ee}} [GeV];Events", 75, 60, 135);
+  TH1D* h_zm_mumu_os_data = new TH1D ("h_zm_mumu_os_data", ";#it{m}_{#it{#mu#mu}} [GeV];Counts", 90, 60, 150);
+  h_zm_mumu_os_data->Sumw2 ();
+  TH1D* h_zm_mumu_ss_data = new TH1D ("h_zm_mumu_ss_data", ";#it{m}_{#it{#mu#mu}} [GeV];Counts", 90, 60, 150);
+  h_zm_mumu_ss_data->Sumw2 ();
+  TH1D* h_zm_mumu_os_mc = new TH1D ("h_zm_mumu_os_mc", ";#it{m}_{#it{#mu#mu}} [GeV];Counts", 90, 60, 150);
+  h_zm_mumu_os_mc->Sumw2 ();
+  TH1D* h_zm_mumu_ss_mc = new TH1D ("h_zm_mumu_ss_mc", ";#it{m}_{#it{#mu#mu}} [GeV];Counts", 90, 60, 150);
+  h_zm_mumu_ss_mc->Sumw2 ();
+
+  TH1D* h_zm_ee_os_data = new TH1D ("h_zm_ee_os_data", ";#it{m}_{#it{ee}} [GeV];Counts", 90, 60, 150);
+  h_zm_ee_os_data->Sumw2 ();
+  TH1D* h_zm_ee_ss_data = new TH1D ("h_zm_ee_ss_data", ";#it{m}_{#it{ee}} [GeV];Counts", 90, 60, 150);
+  h_zm_ee_ss_data->Sumw2 ();
+  TH1D* h_zm_ee_qcd_data = new TH1D ("h_zm_ee_qcd_data", ";#it{m}_{#it{ee}} [GeV];Counts", 90, 60, 150);
+  h_zm_ee_qcd_data->Sumw2 ();
+  TH1D* h_zm_ee_os_mc = new TH1D ("h_zm_ee_os_mc", ";#it{m}_{#it{ee}} [GeV];Counts", 90, 60, 150);
+  h_zm_ee_os_mc->Sumw2 ();
+  TH1D* h_zm_ee_ss_mc = new TH1D ("h_zm_ee_ss_mc", ";#it{m}_{#it{ee}} [GeV];Counts", 90, 60, 150);
+  h_zm_ee_ss_mc->Sumw2 ();
+  //TH1D* h_zm_ee_qcd_mc = new TH1D ("h_zm_ee_qcd_mc", ";#it{m}_{#it{ee}} [GeV];Counts", 90, 60, 150);
+  //h_zm_ee_qcd_mc->Sumw2 ();
 
   for (int iEvt = 0; iEvt < dataTree->GetEntries (); iEvt++) {
     dataTree->GetEntry (iEvt);
 
     if (!isEE) {
+      if (isSameSign) h_zm_mumu_ss_data->Fill (z_m);
+      else            h_zm_mumu_os_data->Fill (z_m);
       if (76 < z_m && z_m < 106) {
-        if (isSameSign) n_ss_mumu += event_weight;
-        else            n_os_mumu += event_weight;
+        if (isSameSign) n_ss_mumu += 1;
+        else            n_os_mumu += 1;
       }
     }
 
     else {
-      if (isQCDBkg)         h_zm_ee_qcd_data->Fill (z_m, event_weight);
-      else if (isSameSign)  h_zm_ee_ss_data->Fill (z_m, event_weight);
-      else if (!isSameSign) h_zm_ee_os_data->Fill (z_m, event_weight);
+      if (isQCDBkg)         h_zm_ee_qcd_data->Fill (z_m);
+      else if (isSameSign)  h_zm_ee_ss_data->Fill (z_m);
+      else if (!isSameSign) h_zm_ee_os_data->Fill (z_m);
     }
   }
-
-  const float muonQCDBkgRate = n_ss_mumu / (n_ss_mumu + n_os_mumu);
-  cout << "QCD bkg in Z->mumu = " << muonQCDBkgRate * 100 << "%" << endl;
-
 
   for (int iEvt = 0; iEvt < mcTree->GetEntries (); iEvt++) {
     mcTree->GetEntry (iEvt);
 
     if (isEE) {
-      if (isQCDBkg)         h_zm_ee_qcd_mc->Fill (z_m, event_weight);
-      else if (isSameSign)  h_zm_ee_ss_mc->Fill (z_m, event_weight);
-      else if (!isSameSign) h_zm_ee_os_mc->Fill (z_m, event_weight);
+      if (isSameSign) h_zm_ee_ss_mc->Fill (z_m);
+      else            h_zm_ee_os_mc->Fill (z_m);
+    }
+    else {
+      if (isSameSign) h_zm_mumu_ss_mc->Fill (z_m);
+      else            h_zm_mumu_os_mc->Fill (z_m);
     }
   }
 
 
-  const float mcNormFactor = h_zm_ee_os_data->Integral () / h_zm_ee_os_mc->Integral ();
-  h_zm_ee_qcd_mc->Scale (mcNormFactor);
-  h_zm_ee_ss_mc->Scale (mcNormFactor);
-  h_zm_ee_os_mc->Scale (mcNormFactor);
+  h_zm_mumu_ss_data->Rebin (2);
+  h_zm_mumu_ss_mc->Rebin (2);
+  const float mumuMCNormFactor = h_zm_mumu_os_data->Integral () / h_zm_mumu_os_mc->Integral ();
+  h_zm_mumu_ss_mc->Scale (mumuMCNormFactor);
+  h_zm_mumu_os_mc->Scale (mumuMCNormFactor);
 
+  const float muonQCDBkgRate = n_ss_mumu / (n_ss_mumu + n_os_mumu);
+  cout << "QCD bkg in Z->mumu = " << muonQCDBkgRate * 100 << "%" << endl;
+
+
+  h_zm_ee_ss_data->Rebin (2);
+  h_zm_ee_qcd_data->Rebin (2);
+  h_zm_ee_ss_mc->Rebin (2);
+  const float eeMCNormFactor = h_zm_ee_os_data->Integral () / h_zm_ee_os_mc->Integral ();
+  h_zm_ee_ss_mc->Scale (eeMCNormFactor);
+  h_zm_ee_os_mc->Scale (eeMCNormFactor);
 
   TH1D* h_zm_ee_ss_data_minus_mc = (TH1D*) h_zm_ee_ss_data->Clone ("h_zm_ee_ss_data_minus_mc");
   h_zm_ee_ss_data_minus_mc->Add (h_zm_ee_ss_mc, -1);
@@ -89,6 +120,102 @@ void CalcQCDBkgs () {
   
   const float electronQCDBkgRate = h_zm_ee_qcd_data->Integral (h_zm_ee_qcd_data->FindBin (76), h_zm_ee_qcd_data->FindBin (106) - 1) / h_zm_ee_os_data->Integral (h_zm_ee_os_data->FindBin (76), h_zm_ee_os_data->FindBin (106) - 1);
   cout << "QCD bkg in Z->ee   = " << electronQCDBkgRate * 100 << "%" << endl;
+
+  TH1D* h_zm_ee_os_mc_plus_qcd = (TH1D*) h_zm_ee_os_mc->Clone ("h_zm_ee_os_mc_plus_qcd");
+  for (int iX = 1; iX <= h_zm_ee_os_mc_plus_qcd->GetNbinsX (); iX++) {
+    h_zm_ee_os_mc_plus_qcd->SetBinContent (iX, h_zm_ee_os_mc_plus_qcd->GetBinContent (iX) + h_zm_ee_qcd_data->GetBinContent (h_zm_ee_qcd_data->FindBin (h_zm_ee_os_mc_plus_qcd->GetBinCenter (iX))));
+  }
+
+
+
+
+  TCanvas* c_zm_mumu = new TCanvas ("c_zm_mumu", "", 600, 600);
+  gPad->SetLogy ();
+  h_zm_mumu_os_mc->SetFillColor (kYellow-9);
+  h_zm_mumu_ss_mc->SetLineColor (kAzure-6);
+  h_zm_mumu_ss_mc->SetLineStyle (2);
+  h_zm_mumu_os_data->SetMarkerColor (kBlack);
+  h_zm_mumu_os_data->SetLineColor (kBlack);
+  h_zm_mumu_os_data->SetMarkerStyle (kFullCircle);
+  h_zm_mumu_os_data->SetMarkerSize (0.8);
+  h_zm_mumu_ss_data->SetMarkerColor (kRed+1);
+  h_zm_mumu_ss_data->SetLineColor (kRed+1);
+  h_zm_mumu_ss_data->SetMarkerStyle (kFullCircle);
+  h_zm_mumu_ss_data->SetMarkerSize (0.8);
+
+  h_zm_mumu_os_mc->GetYaxis ()->SetRangeUser (0.01, 3e4);
+  h_zm_mumu_os_mc->GetXaxis ()->SetLabelSize (0.9*h_zm_mumu_os_mc->GetXaxis ()->GetLabelSize ());
+  h_zm_mumu_os_mc->GetYaxis ()->SetLabelSize (0.9*h_zm_mumu_os_mc->GetYaxis ()->GetLabelSize ());
+
+  h_zm_mumu_os_mc->Draw ("hist");
+  h_zm_mumu_ss_mc->Draw ("hist same");
+  h_zm_mumu_os_data->Draw ("e1 same");
+  h_zm_mumu_ss_data->Draw ("e1 same");
+
+  myText (0.20, 0.89, kBlack, "#bf{#it{ATLAS}} Internal", 0.042);
+  myText (0.20, 0.84, kBlack, "Pb+Pb, #sqrt{s_{NN}} = 5.02 TeV", 0.036);
+  myText (0.20, 0.79, kBlack, "2018 data, 1.4 nb^{-1}", 0.036);
+
+  myMarkerText  (0.67, 0.88, kBlack, kFullCircle, "Opposite-sign data", 0.8, 0.032);
+  myOnlyBoxText (0.67, 0.84, 1, kYellow-9, kBlack, 1, "Opposite-sign MC", 0.032, 1001, 1);
+  myMarkerText  (0.67, 0.80, kRed+1, kFullCircle, "Same-sign data", 0.8, 0.032);
+  myLineText    (0.67, 0.76, kAzure-6, 2, "Same-sign MC", 1, 0.032);
+
+  c_zm_mumu->SaveAs ("Plots/ZmumuQCDBkg.pdf");
+  
+
+
+
+
+  TCanvas* c_zm_ee = new TCanvas ("c_zm_ee", "", 600, 600);
+  gPad->SetLogy ();
+  h_zm_ee_os_mc->SetFillColor (kYellow-9);
+  h_zm_ee_ss_mc->SetLineColor (kAzure-6);
+  h_zm_ee_ss_mc->SetLineStyle (2);
+  h_zm_ee_os_data->SetMarkerColor (kBlack);
+  h_zm_ee_os_data->SetLineColor (kBlack);
+  h_zm_ee_os_data->SetMarkerStyle (kFullCircle);
+  h_zm_ee_os_data->SetMarkerSize (0.8);
+  h_zm_ee_ss_data->SetMarkerColor (kRed+1);
+  h_zm_ee_ss_data->SetLineColor (kRed+1);
+  h_zm_ee_ss_data->SetMarkerStyle (kFullCircle);
+  h_zm_ee_ss_data->SetMarkerSize (0.8);
+  h_zm_ee_qcd_data->SetLineColor (kGreen+3);
+  h_zm_ee_ss_data_minus_mc->SetMarkerStyle (kOpenCircle);
+  h_zm_ee_ss_data_minus_mc->SetMarkerSize (0.8);
+  h_zm_ee_ss_data_minus_mc->SetMarkerColor (kMagenta);
+  h_zm_ee_ss_data_minus_mc->SetLineColor (kMagenta);
+  h_zm_ee_os_mc_plus_qcd->SetLineColor (kRed+1);
+  h_zm_ee_os_mc_plus_qcd->SetLineStyle (2);
+
+  h_zm_ee_os_mc->GetYaxis ()->SetRangeUser (0.08, 2e4);
+  h_zm_ee_os_mc->GetXaxis ()->SetLabelSize (0.9*h_zm_ee_os_mc->GetXaxis ()->GetLabelSize ());
+  h_zm_ee_os_mc->GetYaxis ()->SetLabelSize (0.9*h_zm_ee_os_mc->GetYaxis ()->GetLabelSize ());
+
+  h_zm_ee_os_mc->Draw ("hist");
+  h_zm_ee_ss_mc->Draw ("hist same");
+  h_zm_ee_os_data->Draw ("e1 same");
+  h_zm_ee_ss_data->Draw ("e1 same");
+  h_zm_ee_qcd_data->Draw ("hist same");
+  h_zm_ee_ss_data_minus_mc->Draw ("e1 same");
+  h_zm_ee_os_mc_plus_qcd->Draw ("hist same");
+
+  myText (0.20, 0.89, kBlack, "#bf{#it{ATLAS}} Internal", 0.042);
+  myText (0.20, 0.84, kBlack, "Pb+Pb, #sqrt{s_{NN}} = 5.02 TeV", 0.036);
+  myText (0.20, 0.79, kBlack, "2018 data, 1.7 nb^{-1}", 0.036);
+
+  myMarkerText  (0.67, 0.88, kBlack, kFullCircle, "Opposite-sign data", 0.8, 0.032);
+  myOnlyBoxText (0.67, 0.84, 1, kYellow-9, kBlack, 1, "Opposite-sign MC", 0.032, 1001, 1);
+  myMarkerText  (0.67, 0.80, kRed+1, kFullCircle, "Same-sign data", 0.8, 0.032);
+  myLineText    (0.67, 0.76, kAzure-6, 2, "Same-sign MC", 1, 0.032);
+  myMarkerText  (0.67, 0.72, kMagenta, kOpenCircle, "SS data #minus MC", 0.8, 0.032);
+  myLineText    (0.67, 0.68, kGreen+3, 1, "QCD template", 1, 0.032);
+  myLineText    (0.67, 0.64, kRed+1, 2, "MC Sig. + QCD Bkg.", 1, 0.032);
+
+  c_zm_ee->SaveAs ("Plots/ZeeQCDBkg.pdf");
+
+
+
   
 }
 
